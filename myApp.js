e8b9e052999c02ad0f84a763e60442dd9cba66e0
hermannleboss <hermannleboss@gmail.com>
2023-08-26 10:13:07 +0000

    initiate replit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 let express = require('express');
 let app = express();
+console.log("Hello World")
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
 
 
@@ -35,5 +39,4 @@ let app = express();
 
 
 
-
- module.exports = app;
+module.exports = app;
