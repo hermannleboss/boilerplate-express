426d9e8b252f39897ae625a4f5bd3929fa4cd849
Hermann EHO <hermannleboss@gmail.com>
2023-09-09 17:12:14 +0000

    feat: Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -2,50 +2,33 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/index.html")
 })
 
 // Serve Static Assets
 app.use('/public', express.static(__dirname + "/public"))
 
-// Serve JSON on a Specific Route
-app.get('/json', (req, res) => {
-  let message = "Hello json"
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase()
-  }
-  res.send({ "message": message })
+/**
+Build a simple logger. For every request, it should log to the console a string taking the following format: method path
+ - ip. An example would look like this: GET /json - ::ffff:127.0.0.1. Note that there is a space between method and path
+  and that the dash separating path and ip is surrounded by a space on both sides. You can get the request method (http verb),
+   the relative route path, and the caller’s ip from the request object using req.method, req.path and req.ip. Remember
+    to call next() when you are done, or your server will be stuck forever. Be sure to have the ‘Logs’ opened, and see what
+     happens when some request arrives.
+ */
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Serve JSON on a Specific Route
+app.get('/json', (req, res) => {
+    let message = "Hello json"
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase()
+    }
+    res.send({"message": message})
+})
 
 
 module.exports = app;
